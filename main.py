2ea0c946d86ef181489d7295708939660d0bb28f
Jackson Dias <81438372+JackTrooll29@users.noreply.github.com>
2022-05-10 18:04:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-key = 'cc860da30d290ee2503a4cff2335ecfa'
+key = ''
 cidade = input('Digite o nome da sua cidade: ').lower()
 link = f'https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={key}&lang=pt_br'
 
